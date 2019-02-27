1394db83b62536e0f474d61daef4b404e55ff20c
Ilya Subota <ilyasubota777@gmail.com>
2019-02-27 17:16:36 +0300

    create react app

---
+++ react-app/src/index.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import './media.css';
+import './script.js';
+import './img/..';
+
+
+
+class App extends React.Component {
+    openMmenu(event){
+        let menu = document.getElementById('m-menu');
+        event.preventDefault();
+        menu.classList.toggle('m-menu__active');
+    }
+    closeMmenu(event){
+        let menu = document.getElementById('m-menu');
+        event.preventDefault();
+        menu.classList.toggle('m-menu__active');
+    }
+    searchInput(event){
+        event.preventDefault();
+        let formSearches = document.querySelectorAll('.input-search');
+        [].forEach.call(formSearches, function(item){
+            if(item.style.display === 'flex'){
+                item.style.display = 'none';
+            } else if(item.style.display === 'none'){
+                item.style.display = 'flex';
+            }
+        });
+    }
+    render() {
+        return (
+            <>
+                <ul className="m-menu" id="m-menu">
+                    <li className="close-menu" onClick={ this.closeMmenu }>&times;</li>
+                    <li><a href="#" className="menu-link">Affiche</a></li>
+                    <li><a href="#" className="menu-link">Cinemas</a></li>
+                    <li><a href="#" className="menu-link">Food and Drinks</a></li>
+                    <li><a href="#" className="menu-link">Info</a></li>
+                    <li>
+                        <form className="form-search">
+                            <input type="text" placeholder="Поиск по сайту" className="input-search" />
+                        </form>
+                    </li>
+                    <li><i className="fa fa-search search-icon" style={{color: 'rgba(255, 255, 255, 1)'}} onClick={ this.searchInput }></i></li>
+                    <li><a href="#" className="sign-in-btn menu-link">Sign in</a></li>
+                </ul>
+                <div className="main" id="main">
+                    <div className="header">
+                        <div className="logo">
+                            <a href="#"> <img src="../src/img/logo-min.png" alt="logo" /> </a>
+                        </div>
+                        <a href="#" className="m-menu-link" onClick={ this.openMmenu }>
+                            <i className="fa fa-bars"></i>	
+                        </a>
+                        <div className="menu visible-lg">
+                            <ul><li><a href="#" className="menu-link">Affiche</a></li>
+                            <li><a href="#" className="menu-link">Cinemas</a></li>
+                            <li><a href="#" className="menu-link">Food and Drinks</a></li>
+                            <li><a href="#" className="menu-link">Info</a></li>
+                            <li>
+                                <form className="form-search">
+                                    <input type="text" placeholder="Поиск по сайту" className="input-search" />
+                                </form>
+                            </li>
+                            <li><i className="fa fa-search search-icon"></i></li>
+                            <li><a href="#" className="sign-in-btn menu-link">Sign in</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="slider">
+                        <div className="item item-1"><span>Film 1</span></div>
+                        <div className="item item-2"><span>Film 2</span></div>
+                        <div className="item item-3"><span>Film 3</span></div>
+                        <div className="item item-4"><span>Film 4</span></div>
+                    </div>
+                    <a href="#table" className="main-arrow">
+                        <div className="arrow-down" id="arrow-down">
+                            <svg height="100%" viewBox="0 0 24 24" width="100%"><path d="M7.41,8.59L12,13.17l4.59-4.58L18,10l-6,6l-6-6L7.41,8.59z" fill="#fff"></path></svg>
+                        </div>
+                    </a>
+                </div>
+            </>
+        ); 
+    }
+}
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
--- react-app/src/script.js
+++ react-app/src/script.js
@@ -1,24 +1,13 @@
-var link = document.getElementById('m-menu-link');
-var close = document.getElementById('close-menu');
-var menu = document.getElementById('m-menu');
-link.addEventListener('click', function(event) {
-    event.preventDefault();
-    menu.classList.toggle('m-menu__active');
-});
-close.addEventListener('click', function(event) {
-    event.preventDefault();
-    menu.classList.toggle('m-menu__active');
-});
 
 var searches = document.querySelectorAll('.search-icon');	
 var formSearches = document.querySelectorAll('.input-search');
 [].forEach.call(searches, function(i){
-    i.addEventListener('click', function(){
-        event.preventDefault();
+    i.addEventListener('click', function(i){
+        i.preventDefault();
         [].forEach.call(formSearches, function(item){
-            if(item.style.display == 'flex'){
+            if(item.style.display === 'flex'){
                 item.style.display = 'none';
-            } else if(item.style.display = 'none'){
+            } else if(item.style.display === 'none'){
                 item.style.display = 'flex';
             }
         });
@@ -28,40 +17,40 @@ var formSearches = document.querySelectorAll('.input-search');
 var signInBtns = document.querySelectorAll('.sign-in-btn');
 var formAuthorization = document.getElementById('form-authorization');
 [].forEach.call(signInBtns, function(e){
-    e.addEventListener('click', function(){
-        event.preventDefault();
+    e.addEventListener('click', function(e){
+        e.preventDefault();
         formAuthorization.setAttribute('opened', 'true');
         formRegistration.removeAttribute('opened');
     });
 });
 formAuthorization.addEventListener('click', function(e){
-    if(this == e.target){
+    if(this === e.target){
         formAuthorization.removeAttribute('opened');
     }
 });
 var regBtns = document.querySelectorAll('.reg-btn');
 var formRegistration = document.getElementById('form-registration');
 var signInLink = document.querySelector('.auto-link');
-signInLink.addEventListener('click', function(){
-    event.preventDefault();
+signInLink.addEventListener('click', function(e){
+    e.preventDefault();
     formAuthorization.setAttribute('opened', 'true');
     formRegistration.removeAttribute('opened');
 });
 [].forEach.call(regBtns, function(e){
-    e.addEventListener('click', function(){
-        event.preventDefault();
+    e.addEventListener('click', function(e){
+        e.preventDefault();
         formRegistration.setAttribute('opened', 'true');
         formAuthorization.removeAttribute('opened');
     });
 });
 formRegistration.addEventListener('click', function(e){
-    if(this == e.target){
+    if(this === e.target){
         formRegistration.removeAttribute('opened');
     }
 });
 
 window.addEventListener('keydown', function(e){
-    if(e.key == 'Escape'){
+    if(e.key === 'Escape'){
         formAuthorization.removeAttribute('opened');
         formRegistration.removeAttribute('opened');
     }
