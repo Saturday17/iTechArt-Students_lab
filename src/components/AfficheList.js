cc9237ea2ac4a6bded519be5291c394a02dff06f
Ilya Subota <ilyasubota777@gmail.com>
2019-03-13 19:19:21 +0300

    Add TheMovieDB to the project

--- src/components/AfficheList.js
+++ src/components/AfficheList.js
@@ -1,41 +1,47 @@
 import React, {Component} from 'react';
 import Article from './Article';
-import articles from './films';
 import $ from 'jquery';
 import uniqueId from 'lodash.uniqueid';
 
 class AfficheList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {}
+
+    this.performSearch();
+  }
+
   performSearch() { 
     console.log('Perform search using moviedb');
-    const urlString = 'https://api.themoviedb.org/3/search/movie?api_key=0db50d1e81184cc04e761a3e55b0ee62&language=en-US&query=marvel&page=1&include_adult=false'; 
+    const urlString = 'https://api.themoviedb.org/3/search/movie?api_key=0db50d1e81184cc04e761a3e55b0ee62&language=en-US&query=woman&page=1&include_adult=false'; 
     $.ajax({ 
         url: urlString, 
         success: searchResults => { 
-          console.log("success!"); 
-          console.log(searchResults);
-          const results = searchResults.results;
-          console.log(results[0]);
+          console.log("success!");
+          const movies = searchResults.results;
           var movieRows = [];
-
-          results.forEach( movie => {
-              console.log(movie.title);
-              const movieRow = <Article movie={movie} key={movie.id} />;
-              movieRows.push(movieRow);
-          }) 
-          this.setState()
+          movies.forEach( movie => {
+            movie.poster_src = 'https://image.tmdb.org/t/p/w185' + movie.poster_path;
+            console.log(movie);
+            var movieRow = <Article key={ uniqueId('movie_') } movie={ movie }/>;
+            movieRows.push(movieRow);
+          })
+          this.setState({
+            rows: movieRows
+          })
         }, 
         error: (xhr, status, err) => { 
             console.error('faild!'); 
         } 
     }) 
   }
+
   render() {
-    this.performSearch();
+    const { rows } = this.state
     return (
       <div className="price-tags">
-        { 
-          articles.map(article => <Article article={article} key={uniqueId('movie_')} />) 
-        }
+        { rows }
       </div>
     );
   }
--- src/components/PremiereList.js
+++ src/components/PremiereList.js
@@ -5,9 +5,6 @@ import articles from './films';
 function PremiereList() {
   return (
     <div className="price-tags">
-      <Article article={articles[0]} key={articles.id} />
-      <Article article={articles[1]} key={articles.id} />
-      <Article article={articles[2]} key={articles.id} />
     </div>
   );
 }
