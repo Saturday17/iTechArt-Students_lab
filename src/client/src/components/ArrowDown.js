fde46667a301769716b42bb9155b68cc16f77d09
Ilya Subota <ilyasubota777@gmail.com>
2019-04-18 02:38:42 +0300

    +styles

--- src/client/src/components/ArrowDown.js
+++ src/client/src/components/ArrowDown.js
@@ -4,7 +4,7 @@ function ArrowDown() {
   return (
     <a href="#table" className="main-arrow">
       <div className="arrow-down" id="arrow-down">
-          <svg height="100%" viewBox="0 0 24 24" width="100%"><path d="M7.41,8.59L12,13.17l4.59-4.58L18,10l-6,6l-6-6L7.41,8.59z" fill="#fff"></path></svg>
+          <svg height="100%" viewBox="0 0 24 24" width="100%"><path d="M7.41,8.59L12,13.17l4.59-4.58L18,10l-6,6l-6-6L7.41,8.59z" fill="#000"></path></svg>
       </div>
     </a>
   );
